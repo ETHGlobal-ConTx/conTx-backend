861a8c5c76a8db2b5af230b45e3d8a8e57b152ab
Mohammad Ranjbar Z <mranjbar.z2993@gmail.com>
2023-11-18 19:18:28 +0300

    Add webservice for uploading files to ipfs

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const port = 3003;
+const fs = require('fs')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 require('dotenv').config(); // Make sure to require dotenv if you're using a .env file
 const {initDb} = require('../db/setup');
-const { createNewAttestationRecord} = require("../attestation/attestation");
-const {findMetadatasWithTxHashes, createOrUpdateMetadata, findMetadataWithTxHash} = require("../db/repositories/metadataRepository");
-
+const {createNewAttestationRecord} = require("../attestation/attestation");
+const {
+  findMetadatasWithTxHashes,
+  createOrUpdateMetadata,
+  findMetadataWithTxHash
+} = require("../db/repositories/metadataRepository");
+const {pinFile} = require("../utils/utils");
 
 
 const addRoutes = () => {
   app.post('/metadata', async (req, res) => {
     console.log('req.body', req.body);
     const body = req.body
-    const result = await createOrUpdateMetadata({...body, timestamp: new Date().getTime()} )
+    const result = await createOrUpdateMetadata({...body, timestamp: new Date().getTime()})
     res.send(result);
   });
 
@@ -25,9 +32,9 @@ const addRoutes = () => {
     try {
       console.log('req.body', req.body);
       const body = req.body
-      const {attestationChain,txHash } = body
+      const {attestationChain, txHash} = body
       const metadata = await findMetadataWithTxHash(txHash)
-      if (!metadata){
+      if (!metadata) {
         throw new Error("There isn't any metadata in the DB with this txHash")
       }
       const attestationHash = await createNewAttestationRecord({
@@ -64,9 +71,39 @@ const addRoutes = () => {
   })
 
 
-  app.post('/login',(req, res)=>{
+  app.post('/login', (req, res) => {
     return res.send('Success')
-  } )
+  })
+
+  app.post('/ipfs', upload.single('file'), async (req, res) => {
+    if (!req.file) {
+      return res.status(400).send('No file uploaded.');
+    }
+
+    try {
+      const filePath = req.file.path; // Corrected file path from multer's req.file object
+
+      // Call your function to pin the file, passing the read stream and other necessary info
+      const response = await pinFile(
+        filePath
+      );
+
+      // Cleanup: Delete the file after uploading to IPFS
+      fs.unlink(filePath, (err) => { // Corrected to async file deletion with error handling
+        if (err) {
+          console.error('Error deleting file:', err);
+        }
+      });
+
+      // Return the IPFS link
+      return res.send(`${process.env.PINATA_GATEWAY_ADDRESS}/ipfs/${response.data.IpfsHash}`);
+    } catch (e) {
+      console.error('upload() error', e);
+      res.status(400).send(e.message);
+    }
+  });
+
+
 }
 
 const initServer = async () => {
@@ -80,15 +117,10 @@ const initServer = async () => {
   // initialize app to set middlewares and lisren to port
   app.use(cors());
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({extended: true}));
   addRoutes();
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 }
-
-
-
-
-
 
 module.exports = {
   initServer
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,7 @@
+const FormData = require('form-data');
+const Axios = require('axios');
+const fs = require('fs');
+
 // Helper function to create a hash
 function createHash(obj) {
   const jsonString = JSON.stringify(obj);
@@ -13,12 +17,50 @@ const getAttestationAddressesPerChain = (chainId) => {
         easContractAddress: "0x4200000000000000000000000000000000000021",
         schemaUID: "0x4dc1609b439db1b2d52c83ca334adf8837da74294d66dd967a7f8d0f79b07b95"
       };
+    case 'arbGoerli':
+      return {
+        infuraPrefix: 'https://arbitrum-goerli.infura.io/v3/',
+        easContractAddress: "0xaEF4103A04090071165F78D45D83A0C0782c2B2a",
+        schemaUID: "0x4dc1609b439db1b2d52c83ca334adf8837da74294d66dd967a7f8d0f79b07b95"
+      };
+    case 'lineaGoerli':
+      return {
+        infuraPrefix: 'https://linea-goerli.infura.io/v3/',
+        easContractAddress: "0xaEF4103A04090071165F78D45D83A0C0782c2B2a",
+        schemaUID: "0x4dc1609b439db1b2d52c83ca334adf8837da74294d66dd967a7f8d0f79b07b95"
+      };
     default:
       throw new Error('Invalid chain')
 
   }
+
+
 }
+
+const pinFile = (filePath) => {
+  console.log('pinFile() filePath ', filePath)
+
+  // Check if the file exists and is readable
+  if (!fs.existsSync(filePath)) {
+    throw new Error('File does not exist');
+  }
+  console.log('pinFile(), file exists')
+  const createReadStream =  fs.createReadStream(filePath); // Corrected to use the file path
+  const data = new FormData();
+  data.append('file', createReadStream);
+  return Axios.post('https://api.pinata.cloud/pinning/pinFileToIPFS', data, {
+    maxContentLength: Infinity, // Needed to prevent Axios from throwing error with large files
+    headers: {
+      ...data.getHeaders(), // Appends the necessary Content-Type header with the boundary
+      pinata_api_key: process.env.PINATA_API_KEY,
+      pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+    },
+  });
+};
+
+
 module.exports = {
   createHash,
-  getAttestationAddressesPerChain
+  getAttestationAddressesPerChain,
+  pinFile
 }
